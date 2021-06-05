c2421b080cbdd7004cd5a5c3632b978e80a7007e
S.PRANAVKUMAR <77280162+pranavfreefire@users.noreply.github.com>
2021-06-05 17:34:00 +0530

    Add files via upload

---
+++ JS/player.js
@@ -0,0 +1,31 @@
+class Player{
+constructor(){
+this.index=null
+this.name=null
+this.distance=0
+}
+getCount(){
+    database.ref('playerCount').on('value',(data)=>{
+    playerCount=data.val()
+    })
+    }
+    updateCount(x){
+        database.ref('/').update({
+            playerCount:x
+        })
+        }
+         update() {
+            var path="players/player"+player.index
+            database.ref(path).update({
+                name:this.name,
+                distance:this.distance
+            })
+        }
+         getplayerinfo(){
+             database.ref('players').on("value",(data)=>{
+                 allPlayers=data.val()
+             })
+         }
+        }
+
+
